67d4788ad3cf1bd04bdd70730a8171de9fc1fe92
Wlad <yanushevskyv@gmail.com>
2020-10-03 13:12:27 +0300

    solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array) return 0;
+    let minElement = array[0];
+    for(let i=0;i<array.length;i++){
+        if(minElement > array[i]){
+           minElement = array[i]
+        }
+  }
+  return minElement || 0 ;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array) return 0;
+    let maxElement = array[0];
+    for(let i=0;i<array.length;i++){
+        if(maxElement < array[i]){
+           maxElement = array[i]
+        }
+  }
+    return maxElement || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array) return 0;
+    const average = array => array.reduce( ( p, c ) => p + c, 0 ) / array.length;
+    const result = average( array );
+  return result ||0;
 }
